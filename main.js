3ac9720e59a87d2df49713ae6e498d0ca59acac0
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-09-21 17:44:34 +0000

    javascript populates the html elements with data from the object

---
+++ main.js
@@ -0,0 +1,48 @@
+const containerArray = [
+      {
+        header: "test1",
+        button: "test2",
+        bottomText: "test3",
+        resetButton: "test4"
+    }
+    ,
+    {
+        header:"2ndarraytest1",
+        button:"2ndarraytest2",
+        bottomText: "2ndarraytest3",
+        resetButton: "2ndarraytest4"
+    }
+    ,
+    {
+        header: "placeholder",
+        button: "placeholder",
+        bottomtext: "placeholder",
+        resetbutton: "placeholder"
+    }
+    ,
+    {
+        header: "placeholder",
+        button: "placeholder",
+        bottomtext: "placeholder",
+        resetbutton: "placeholder"
+    }
+    ,
+    {
+        header: "placeholder",
+        button: "placeholder",
+        bottomtext: "placeholder",
+        resetbutton: "placeholder"
+    }
+    ,
+    {
+        header: "placeholder",
+        button: "placeholder",
+        bottomtext: "placeholder",
+        resetbutton: "placeholder"
+    }
+]
+   
+document.getElementById('header').innerHTML = containerArray[0].header1;
+document.getElementById('btn').innerHTML = containerArray[0].button1;
+document.getElementById('bottomtext').innerHTML = containerArray[0].bottomText1;
+document.getElementById('rstbtn').innerHTML = containerArray[0].resetButton1;
