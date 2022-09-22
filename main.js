1ad436a223fc5b8182e7e4d3a2bdd212476151e3
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-09-22 02:36:04 +0000

    updating code

--- main.js
+++ main.js
@@ -3,46 +3,132 @@ const containerArray = [
         header: "test1",
         button: "test2",
         bottomText: "test3",
-        resetButton: "test4"
+        resetButton: "reset"
     }
     ,
     {
         header:"2ndarraytest1",
-        button:"2ndarraytest2",
-        bottomText: "2ndarraytest3",
-        resetButton: "2ndarraytest4"
+        button:"2ndarraytest1",
+        bottomText: "2ndarraytest1",
+        resetButton: "reset"
     }
     ,
     {
-        header: "placeholder",
-        button: "placeholder",
-        bottomtext: "placeholder",
-        resetbutton: "placeholder"
+        header: "3",
+        button: "3",
+        bottomText: "3",
+        resetButton: "reset"
     }
     ,
     {
-        header: "placeholder",
-        button: "placeholder",
-        bottomtext: "placeholder",
-        resetbutton: "placeholder"
+        header: "4",
+        button: "4",
+        bottomText: "4",
+        resetButton: "reset"
     }
     ,
     {
-        header: "placeholder",
-        button: "placeholder",
-        bottomtext: "placeholder",
-        resetbutton: "placeholder"
+        header: "5",
+        button: "5",
+        bottomText: "5",
+        resetButton: "reset"
     }
     ,
     {
-        header: "placeholder",
-        button: "placeholder",
-        bottomtext: "placeholder",
-        resetbutton: "placeholder"
+        header: "6",
+        button: "6",
+        bottomText: "6",
+        resetButton: "reset"
     }
 ]
+
+
+
+//first mix array
+//s
+//this is straight up stolen from stack overflow
+// function shuffleArray(array) {
+//     for (var i = array.length - 1; i > 0; i--) {
+//         var j = Math.floor(Math.random() * (i + 1));
+//         var temp = array[i];
+//         array[i] = array[j];
+//         array[j] = temp;
+//     }
+//     return array;
+// }
+// let symbol = ["t", "e", "s", "t"]
+
+
+
+// function magic() {
+//     shuffleArray(symbol);
+//     for (i = 0; i < 100 ; i++) {
+//         if (i % 9 === 0) {
+            
+//         }
+//         else {
+//             return i
+//         }
+// }
+// }
+// console.log(magic())
+    
+function pageUpdate () {
+    document.getElementById('header').innerHTML = containerArray[x].header;
+    document.getElementById('btn').innerHTML = containerArray[x].button;
+    document.getElementById('bottomtext').innerHTML = containerArray[x].bottomText;
+    document.getElementById('rstbtn').innerHTML = containerArray[x].resetButton;
+    
+    }
+    pageUpdate();
+
+
+function buttonClickForward (e) {
    
-document.getElementById('header').innerHTML = containerArray[0].header1;
-document.getElementById('btn').innerHTML = containerArray[0].button1;
-document.getElementById('bottomtext').innerHTML = containerArray[0].bottomText1;
-document.getElementById('rstbtn').innerHTML = containerArray[0].resetButton1;
+    ++x
+    if (x > 6) {
+        x = 0
+    } 
+    pageUpdate()
+}
+
+function buttonClickBack (e) {
+    --x
+    if (x < 0) {
+        x = 0
+    }
+
+    pageUpdate()
+}
+
+let btnClick = document.getElementById('btn');
+btnClick.addEventListener('click', buttonClickForward)
+
+let btnClickBack = document.getElementById('rstbtn')
+btnClickBack.addEventListener('click', buttonClickBack)
+
+
+// function resetPage () {
+//     document.getElementById('rstbtn').innerHTML = containerArray[x].resetButton;
+// }
+// resetPage();
+
+// let rstClick = document.getElementById('rstbtn');
+// rstClick.addEventListener('click', handleReset);
+
+
+// function pageUpdate () {
+// document.getElementById('header').innerHTML = containerArray[x].header;
+// document.getElementById('btn').innerHTML = containerArray[x].button;
+// document.getElementById('bottomtext').innerHTML = containerArray[x].bottomText;
+// // document.getElementById('rstbtn').innerHTML = containerArray[x].resetButton;
+
+// }
+// pageUpdate();
+
+
+
+
+
+
+    
