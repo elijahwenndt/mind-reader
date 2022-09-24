5ee3fde306daf926c5646e62f95346be49c819ef
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-09-24 20:00:16 +0000

    updated a few things, removed the algo in favor of a new randomize, fixed my buttons displays to display buttons on proper page, added a zero = solution at this start so numbers < 10 will work

--- main.js
+++ main.js
@@ -42,10 +42,10 @@ const containerArray = [
     ,
     {
         header: " ",
-        button: "try again?",
+        button: " ",
         bottomText: "is this your letter?",
         backButton: "previous page",
-        resetButton: " "
+        resetButton: "try again?"
     }
 ]
 //set up a global variable = 0 
@@ -61,55 +61,55 @@ function pageUpdate () {
     document.getElementById('back').innerHTML = containerArray[x].backButton;
     document.getElementById('resetbtn').innerHTML = containerArray[x].resetButton;
     
-    if (x === 0 || x === 5){
-        back.classList.add("d-none");
-        resetbtn.classList.add('d-none')
-        // reset.style.display = 'none'
+    
+    if (x === 0) {
+        back.classList.add('d-none');
+        resetbtn.classList.add('d-none');
+        btn.classList.remove('d-none')  
+    }
+    else if (x === 5) {
+        resetbtn.classList.remove('d-none');
+        btn.classList.add('d-none')
+        back.classList.add('d-none')
+        
     }
     else {
         back.classList.remove('d-none')
         resetbtn.classList.remove('d-none')
+        btn.classList.remove('d-none')
     }
     
+
     }
 
     //runs the function directly after setting it up. because x = 0 at the start, it displays the first page
     pageUpdate();
-    // if (x === 0 || x === 5){
-    //     back.classList.add("d-none");
-    //     Reset.classList.add('d-none')
-    //     // reset.style.display = 'none'
-    // }
-    // else {
-    //     back.classList.remove('d-none')
-    //     Reset.classList.add('d-none')
-    // }
+   
     
 //set up the click function, when the button gets clicked, increments x by 1. this in turn sets the global variable and thus the index of the containerArray
 //then it updates the page
-function buttonClickForward (e) {
-   
+function buttonClickForward () {
     ++x
     if (x >= 6) {
-        x = 0
+        x = 0  
     } 
     pageUpdate()
 }
 
 //same thing as above but goes back a page
-function buttonClickBack (e) {
+function buttonClickBack () {
     --x
     if (x < 0) {
         x = 0
     }
-
     pageUpdate()
 }
 
 //similar concept as above but instead when the reset button is clicked it sets the global variable back to 0
-function reset (e){
+function reset (){
     x=0
     pageUpdate()
+    magic()
 }
 
 //sets variable = the grabbed button ID
@@ -123,50 +123,37 @@ btnClickBack.addEventListener('click', buttonClickBack)
 let btnReset = document.getElementById('resetbtn')
 btnReset.addEventListener('click', reset)
 
-//a slightly modified version of the Durstenfeld shuffle algorithm
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
-}
 
-//set up the symbol array
 let symbol = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-// let otherSymbol = ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]    old code example
+
 
 
 function magic() {
 
-    //passes the symbol array into the shuffle array function
-    let shuffle = shuffleArray(symbol);
+    let solution = Math.floor(Math.random() * symbol.length)
     
-
-    //sets up empty array 
     let emptyArray = [];
     
-    for (let i = 1; i < 101 ; i++) {
+    for (let i = 0; i < 100 ; i++) {
 
-        //randomized a number and sets equal to x
-        let x = Math.floor(Math.random() * symbol.length); 
+        let filler = Math.floor(Math.random() * symbol.length); 
 
-        //if i is divisible by 9 with no remainder
         if (i % 9 === 0) {
 
             //pushes 
-            emptyArray.push(i + ": " + symbol[1] + '<br>');
+            emptyArray.push(i + ": " + symbol[solution] + '<br>');
         }
         else if (i % 9 !== 0) {
-            emptyArray.push(i + ": " + symbol[x] + '<br>');  
+            emptyArray.push(i + ": " + symbol[filler] + '<br>');  
 
+        }
+        else if (i === 0) {
+            emptyArray.push(i + ": " + symbol[solution] + '<br>');
         }
     
     };
-    containerArray[5].header=symbol[1]
+    containerArray[5].header=symbol[solution]
     containerArray[4].header=emptyArray.join('\r\n')
     return emptyArray;
 }
